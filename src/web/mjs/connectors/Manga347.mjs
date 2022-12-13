ebd0afc7d8ec48655c8e300c2a30d8544b974244
MikeZeDev <MikeZeDev@users.noreply.github.com>
2022-12-13 14:08:05 +0000

    Manga347 : use template
    Its mangastream, i am dumb.

--- src/web/mjs/connectors/Manga347.mjs
+++ src/web/mjs/connectors/Manga347.mjs
@@ -1,23 +1,24 @@
-import Connector from '../engine/Connector.mjs';
-import Manga from '../engine/Manga.mjs';
-export default class Manga347 extends Connector {
+import WordPressMangastream from './templates/WordPressMangastream.mjs';
+export default class Manga347 extends WordPressMangastream {
     constructor() {
         super();
         super.id = 'manga347';
         super.label = 'Manga 347';
         this.tags = ['webtoon', 'english'];
         this.url = 'https://manga347.com';
-    }
-    async _getMangaFromURI(uri) {
-        const request = new Request(uri, this.requestOptions);
-        const data = await this.fetchDOM(request, 'div.anisc-detail h1.manga-name');
-        return new Manga(this, uri.pathname, data[0].textContent.trim());
+        this.path = '/filter';
+        this.queryMangas = 'div.manga-detail h3.manga-name a';
+        this.queryMangaPagesCount = 'ul.pagination li:nth-last-of-type(2) a';
+        this.queryChapters = 'div.chapters-list-ul li.item.reading-item.chapter-item  > a';
+        this.queryPages = 'div#chapter-container img[data-src]';
+        this.queryChaptersTitle = 'span.name';
+        this.querMangaTitleFromURI = 'div.anisc-detail h1.manga-name';
     }
     async _getMangas() {
         let mangaList = [];
         const uri = new URL('/filter', this.url);
         const request = new Request(uri, this.requestOptions);
-        const data = await this.fetchDOM(request, 'ul.pagination li:nth-last-of-type(2) a');
+        const data = await this.fetchDOM(request, this.queryMangaPagesCount);
         const pageCount = parseInt(data[0].text);
         for(let page = 1; page <= pageCount; page++) {
             const mangas = await this._getMangasFromPage(page);
@@ -28,35 +29,12 @@ export default class Manga347 extends Connector {
     async _getMangasFromPage(page) {
         const uri = new URL('/filter/' + page, this.url);
         const request = new Request(uri, this.requestOptions);
-        const data = await this.fetchDOM(request, 'div.manga-detail h3.manga-name a');
+        const data = await this.fetchDOM(request, this.queryMangas);
         return data.map(element => {
             return {
                 id: this.getRootRelativeOrAbsoluteLink(element, this.url),
                 title: element.text.trim()
             };
         });
-    }
-    async _getChapters(manga) {
-        const uri = new URL(manga.id, this.url);
-        const request = new Request(uri, this.requestOptions);
-        const data = await this.fetchDOM(request, 'div.chapters-list-ul li.item.reading-item.chapter-item  > a');
-        return data.map(element => {
-            return {
-                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
-                title: element.title.trim()
-            };
-        });
-    }
-    async _getPages(chapter) {
-        const chapid = chapter.id.match(/\/([0-9]+)$/)[1];
-        const uri = new URL('/ajax/image/chapter/'+chapid, this.url);
-        const request = new Request(uri, this.requestOptions);
-        const response = await this.fetchJSON(request);
-        const data = this.createDOM(response.html).querySelectorAll('source');
-        let pagelist = [];
-        for (const image of data) {
-            pagelist.push(this.getAbsolutePath(image.getAttribute('data-src'), request.url));
-        }
-        return pagelist;
     }
 }
