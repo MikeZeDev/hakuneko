76ca9006364f6d109938becd6b7e268045f61acc
MikeZeDev <MikeZeDev@users.noreply.github.com>
2022-12-13 13:39:31 +0000

    Update Manga347
    Fixes https://github.com/manga-download/hakuneko/issues/4532

--- src/web/mjs/connectors/Manga347.mjs
+++ src/web/mjs/connectors/Manga347.mjs
@@ -1,14 +1,62 @@
-import WordPressMadara from './templates/WordPressMadara.mjs';
-
-export default class Manga347 extends WordPressMadara {
-
+import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
+export default class Manga347 extends Connector {
     constructor() {
         super();
         super.id = 'manga347';
         super.label = 'Manga 347';
         this.tags = ['webtoon', 'english'];
         this.url = 'https://manga347.com';
-
-        this.queryPages = 'figure source, div.page-break source';
+    }
+    async _getMangaFromURI(uri) {
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'meta[property="og:title"]');
+        return new Manga(this, uri.pathname, data[0].content.trim());
+    }
+    async _getMangas() {
+        let mangaList = [];
+        const uri = new URL('/filter', this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'ul.pagination li:nth-last-of-type(2) a');
+        const pageCount = parseInt(data[0].text);
+        for(let page = 1; page <= pageCount; page++) {
+            const mangas = await this._getMangasFromPage(page);
+            mangaList.push(...mangas);
+        }
+        return mangaList;
+    }
+    async _getMangasFromPage(page) {
+        const uri = new URL('/filter/' + page, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'div.manga-detail h3.manga-name a');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.text.trim()
+            };
+        });
+    }
+    async _getChapters(manga) {
+        const uri = new URL(manga.id, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchDOM(request, 'div.chapters-list-ul li.item.reading-item.chapter-item  > a');
+        return data.map(element => {
+            return {
+                id: this.getRootRelativeOrAbsoluteLink(element, this.url),
+                title: element.title.trim()
+            };
+        });
+    }
+    async _getPages(chapter) {
+        const chapid = chapter.id.match(/\/([0-9]+)$/)[1];
+        const uri = new URL('/ajax/image/chapter/'+chapid, this.url);
+        const request = new Request(uri, this.requestOptions);
+        const response = await this.fetchJSON(request);
+        const data = this.createDOM(response.html).querySelectorAll('source');
+        let pagelist = [];
+        for (const image of data) {
+            pagelist.push(this.getAbsolutePath(image.getAttribute('data-src'), request.url));
+        }
+        return pagelist;
     }
 }
