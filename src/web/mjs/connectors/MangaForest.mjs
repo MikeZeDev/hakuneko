3548f6b74e28315524e0e4f08541affae7fd4772
MikeZeDev <MikeZeDev@users.noreply.github.com>
2022-12-24 23:38:02 +0000

    Update MangaForest.mjs

--- src/web/mjs/connectors/MangaForest.mjs
+++ src/web/mjs/connectors/MangaForest.mjs
@@ -2,7 +2,7 @@ import MadTheme from './templates/MadTheme.mjs';
 export default class MangaForest extends MadTheme {
     constructor() {
         super();
-        super.id = 'MangaForest';
+        super.id = 'mangaforest';
         super.label = 'MangaForest';
         this.tags = ['manga', 'webtoon', 'english'];
         this.url = 'https://mangaforest.me';
